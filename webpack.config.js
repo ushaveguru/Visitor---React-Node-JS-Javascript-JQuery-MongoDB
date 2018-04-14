4c96d9240254cfa989db97bd3a70f249a397bad4
Thaijasa <thaijasa@Thaijasas-MacBook-Pro.local>
2018-04-14 16:23:42 -0700

    setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            
+            }        
+        ]
+    }
+};
