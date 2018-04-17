e5e31ff5f45a39888863d8cd688c2fd083051c82
Thaijasa <thaijasa@Thaijasas-MacBook-Pro.local>
2018-04-16 23:44:47 -0700

    Express Completed....

---
+++ config.js
@@ -0,0 +1,5 @@
+const env =process.env;
+export const nodeEnv = env.NODE_ENV ||'development';
+export default {
+    port: env.PORT || 8080
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+
+server.get('/', (req, res)=>{
+   res.send('Hello Express'); 
+});
+
+server.get('/about.html', (req, res) =>{
+    fs.readFile('./about.html', (err, data)=>{
+                res.send(data.toString());
+                });
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, ()=>{
+    console.info('Express listening on port ', config.port);
+});
